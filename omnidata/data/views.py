7a7ce69785b32f24688f519c7ffaa3e4e736a69b
Felix <felixludos.info@gmail.com>
2022-11-02 15:08:02 +0100

    prep views

--- omnidata/data/views.py
+++ omnidata/data/views.py
@@ -1,4 +1,5 @@
 
+from typing import Tuple, List, Dict, Optional, Union, Any, Callable, Sequence, Iterator, Iterable
 
 from .abstract import AbstractView, AbstractBatch, AbstractProgress
 
@@ -22,8 +23,8 @@ class ViewBase(AbstractView):
 
 
 class BatchBase(ViewBase, AbstractBatch):
-	def __init__(self, N=None, *, sel=None, progress=None, **kwargs):
-		super().__init__(**kwargs)
+	def __init__(self, source=None, N=None, *, sel=None, progress=None, **kwargs):
+		super().__init__(source=source, **kwargs)
 		self._progress = progress
 		self._N = N
 		self._sel = sel
@@ -48,8 +49,33 @@ class BatchBase(ViewBase, AbstractBatch):
 class ProgressBase(AbstractProgress):
 	def __init__(self, source, **kwargs):
 		super().__init__(**kwargs)
+		self._source = source
 		self._total_samples = 0
 		self._total_batches = 0
+	
+	@property
+	def source(self):
+		return self._source
+	
+	@property
+	def total_samples(self):
+		return self._total_samples
+	
+	@property
+	def total_batches(self):
+		return self._total_batches
+	
+	def remaining_batches(self) -> Optional[int]:
+		raise NotImplementedError
+	
+	Batch = BatchBase
+	def create_batch(self, N=None, *, sel=None, **kwargs):
+		return self.Batch(N=N, sel=sel, progress=self, source=self.source)
+	
+	
+	def get_next_batch(self):
+		pass
+	
 	
 	pass
 
