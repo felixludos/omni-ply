f81cea96ec4cd04e59bca0d4947166a3a6c93c16
felixludos <felixludos.info@gmail.com>
2023-09-03 16:50:59 +0200

    craftykit gadget order fixed

--- omniply/__init__.py
+++ omniply/__init__.py
@@ -1,15 +1,17 @@
 __version__ = "0.1.1"
 
-from . import util
-from .tools import *
-from .persistent import *
-from .features import *
-from .structure import *
-from .parameters import *
-from .modules import TrainableModel, SimpleModel, Trainer
+from .core import *
+
+# from . import util
+# from .tools import *
+# from .persistent import *
+# from .features import *
+# from .structure import *
+# from .parameters import *
+# from .modules import TrainableModel, SimpleModel, Trainer
 
 # from .models import *
-from .data import *
+# from .data import *
 
 
 
