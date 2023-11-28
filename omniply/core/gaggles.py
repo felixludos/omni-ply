01e01091e1d58008633614c5805d9aac3a51a1f9
felixludos <felixludos.info@gmail.com>
2023-11-28 01:53:33 +0100

    added generated docstrings

--- omniply/core/gaggles.py
+++ omniply/core/gaggles.py
@@ -9,31 +9,90 @@ from .errors import logger, GadgetFailure, MissingGadget, AssemblyError
 from .gadgets import GadgetBase, SingleGadgetBase, FunctionGadget, AutoFunctionGadget
 
 
-
 class GaggleBase(GadgetBase, AbstractGaggle):
-	_gadgets_table: dict[str, list[AbstractGadget]] # tools are kept in O-N order (reversed) for easy updates
+	"""
+	The GaggleBase class is a base class for creating custom gaggles. It uses a protected _gadgets_table dictionary to
+	keep track of all the subgadgets, and consequently implements the expected API for gaggles.
+
+	The gadgets should be in reverse order of presidence (so the last gadget for a given gizmo is tried first).
+
+	Attributes:
+		_gadgets_table (dict[str, list[AbstractGadget]]): A dictionary where keys are gadget names and values are lists
+		of gadgets.
+	"""
+
+	_gadgets_table: dict[str, list[AbstractGadget]]  # A dictionary where keys are gadget names and values are lists of gadgets. The gadgets are kept in O-N order (reversed) for easy updates.
+class GaggleBase(GadgetBase, AbstractGaggle):
+	"""
+	The GaggleBase class is a base class for creating custom gaggles. It uses a protected _gadgets_table dictionary to
+	keep track of all the subgadgets, and consequently implements the expected API for gaggles.
+
+	The gadgets should be in reverse order of presidence (so the last gadget for a given gizmo is tried first).
+
+	Attributes:
+	 _gadgets_table (dict[str, list[AbstractGadget]]): A dictionary where keys are gadget names and values are lists
+  of gadgets.
+	"""
+
+	_gadgets_table: dict[str, list[AbstractGadget]]  # A dictionary where keys are gadget names and values are lists of gadgets. The gadgets are kept in O-N order (reversed) for easy updates.
 
 	def __init__(self, *args, gadgets_table: Optional[Mapping] = None, **kwargs):
+		"""
+		Initializes a new instance of the GaggleBase class.
+
+		Args:
+			args: Variable length argument list.
+			gadgets_table (Optional[Mapping]): A dictionary of gadgets. If not provided, an empty dictionary will be used.
+			kwargs: Arbitrary keyword arguments.
+		"""
 		if gadgets_table is None:
 			gadgets_table = {}
 		super().__init__(*args, **kwargs)
 		self._gadgets_table = gadgets_table
 
-
 	def gizmos(self) -> Iterator[str]:
+		"""
+		Returns an iterator over the keys of the _gadgets_table dictionary.
+
+		Returns:
+			An iterator over the keys of the _gadgets_table dictionary.
+		"""
 		yield from self._gadgets_table.keys()
 
-
 	def grabable(self, gizmo: str) -> bool:
+		"""
+		Checks if a gizmo is in the _gadgets_table dictionary.
+
+		Args:
+			gizmo (str): The name of the gizmo to check.
+
+		Returns:
+			bool: True if the gizmo is in the _gadgets_table dictionary, False otherwise.
+		"""
 		return gizmo in self._gadgets_table
 
-
 	def vendors(self, gizmo: Optional[str] = None) -> Iterator[AbstractGadget]:
-		'''returns all known gadgets that can produce the given gizmo (iterates over local branches)'''
+		"""
+		Returns all known gadgets that can produce the given gizmo. It iterates over local branches.
+
+		Args:
+			gizmo (Optional[str]): The name of the gizmo to check. If not provided, all gadgets are returned.
+
+		Returns:
+			Iterator[AbstractGadget]: An iterator over the gadgets that can produce the given gizmo.
+		"""
 		yield from self._vendors(gizmo)
 
-
 	def _vendors(self, gizmo: Optional[str] = None) -> Iterator[AbstractGadget]:
+		"""
+		Private method that returns all known gadgets that can produce the given gizmo.
+
+		Args:
+			gizmo (Optional[str]): The name of the gizmo to check. If not provided, all gadgets are returned.
+
+		Returns:
+			Iterator[AbstractGadget]: An iterator over the gadgets that can produce the given gizmo.
+		"""
 		if gizmo is None:
 			for gadget in filter_duplicates(chain.from_iterable(map(reversed, self._gadgets_table.values()))):
 				if isinstance(gadget, AbstractGaggle):
@@ -49,9 +108,22 @@ class GaggleBase(GadgetBase, AbstractGaggle):
 				else:
 					yield gadget
 
-
 	_AssemblyFailedError = AssemblyError
 	def grab_from(self, ctx: AbstractGig, gizmo: str) -> Any:
+		"""
+		Tries to grab a gizmo from the context using the gadgets in the _gadgets_table dictionary.
+
+		Args:
+			ctx (AbstractGig): The context from which to grab the gizmo.
+			gizmo (str): The name of the gizmo to grab.
+
+		Returns:
+			Any: The grabbed gizmo.
+
+		Raises:
+			_AssemblyFailedError: If all gadgets fail to produce the gizmo.
+			_MissingGadgetError: If no gadget can produce the gizmo.
+		"""
 		failures = OrderedDict()
 		for gadget in self._vendors(gizmo):
 			try:
@@ -65,19 +137,49 @@ class GaggleBase(GadgetBase, AbstractGaggle):
 			raise self._AssemblyFailedError(failures)
 		raise self._MissingGadgetError(gizmo)
 
-
-
 class LoopyGaggle(GaggleBase):
+	"""
+	The LoopyGaggle class is a subclass of GaggleBase. It uses a protected _grabber_stack dictionary to
+	keep track of all the subgadgets, and consequently implements the expected API for gaggles.
+
+	The gadgets should be in reverse order of presidence (so the last gadget for a given gizmo is tried first).
+
+	Attributes:
+	 _grabber_stack (dict[str, Iterator[AbstractGadget]]): A dictionary where keys are gadget names and values are lists
+  of gadgets.
+	"""
+
 	_grabber_stack: dict[str, Iterator[AbstractGadget]] = None
 
 	def __init__(self, *args, grabber_stack: Optional[Mapping] = None, **kwargs):
+		"""
+		Initializes a new instance of the LoopyGaggle class.
+
+		Args:
+			args: Variable length argument list.
+			grabber_stack (Optional[Mapping]): A dictionary of gadgets. If not provided, an empty dictionary will be used.
+			kwargs: Arbitrary keyword arguments.
+		"""
 		if grabber_stack is None:
 			grabber_stack = {}
 		super().__init__(*args, **kwargs)
 		self._grabber_stack = grabber_stack
 
-
 	def grab_from(self, ctx: 'AbstractGig', gizmo: str) -> Any:
+		"""
+		Tries to grab a gizmo from the context using the gadgets in the _grabber_stack dictionary.
+
+		Args:
+			ctx (AbstractGig): The context from which to grab the gizmo.
+			gizmo (str): The name of the gizmo to grab.
+
+		Returns:
+			Any: The grabbed gizmo.
+
+		Raises:
+			_AssemblyFailedError: If all gadgets fail to produce the gizmo.
+			_MissingGadgetError: If no gadget can produce the gizmo.
+		"""
 		failures = OrderedDict()
 		itr = self._grabber_stack.setdefault(gizmo, self._vendors(gizmo))
 		# should be the same as Kit, except the iterators are cached until the gizmo is produced
@@ -99,15 +201,35 @@ class LoopyGaggle(GaggleBase):
 			raise self._AssemblyFailedError(failures)
 		raise self._MissingGadgetError(gizmo)
 
-
-
 class MutableGaggle(GaggleBase):
+	"""
+	The MutableGaggle class is a subclass of GaggleBase. It provides methods to include and exclude gadgets.
+
+	The gadgets should be in reverse order of presidence (so the last gadget for a given gizmo is tried first).
+	"""
+
 	def include(self, *gadgets: AbstractGadget) -> Self:
-		'''adds given tools in reverse order'''
+		"""
+		Adds given tools in reverse order.
+
+		Args:
+			gadgets (AbstractGadget): The gadgets to be added.
+
+		Returns:
+			Self: The instance of the class with the added gadgets.
+		"""
 		return self.extend(gadgets)
 
-
 	def extend(self, gadgets: Iterable[AbstractGadget]) -> Self:
+		"""
+		Extends the _gadgets_table with the provided gadgets.
+
+		Args:
+			gadgets (Iterable[AbstractGadget]): The gadgets to be added.
+
+		Returns:
+			Self: The instance of the class with the extended gadgets.
+		"""
 		new = {}
 		for gadget in gadgets:
 			for gizmo in gadget.gizmos():
@@ -120,19 +242,34 @@ class MutableGaggle(GaggleBase):
 			self._gadgets_table.setdefault(gizmo, []).extend(reversed(gadgets))
 		return self
 
-
 	def exclude(self, *gadgets: AbstractGadget) -> Self:
-		'''removes the given tools, if they are found'''
+		"""
+		Removes the given tools, if they are found.
+
+		Args:
+			gadgets (AbstractGadget): The gadgets to be removed.
+
+		Returns:
+			Self: The instance of the class with the removed gadgets.
+		"""
 		for gadget in gadgets:
 			for gizmo in gadget.gizmos():
 				if gizmo in self._gadgets_table and gadget in self._gadgets_table[gizmo]:
 					self._gadgets_table[gizmo].remove(gadget)
 		return self
 
-
-
 class CraftyGaggle(GaggleBase, InheritableCrafty):
+	"""
+	The CraftyGaggle class is a subclass of GaggleBase and InheritableCrafty. It provides methods to process crafts.
+
+	The gadgets should be in reverse order of presidence (so the last gadget for a given gizmo is tried first).
+	"""
+
 	def _process_crafts(self):
+		"""
+		Processes the crafts. It groups the crafts by where the craft is defined and converts crafts to skills and adds
+		them in O-N (N-O) order to table.
+		"""
 		# group by where the craft is defined
 		history = OrderedDict() # src<N-O> : craft<N-O>
 		for src, key, craft in self._emit_all_craft_items(): # craft<N-O>
@@ -147,8 +284,6 @@ class CraftyGaggle(GaggleBase, InheritableCrafty):
 					gizmos.setdefault(gizmo, []).append(skill)
 			for gizmo, skills in gizmos.items():
 				self._gadgets_table.setdefault(gizmo, []).extend(reversed(skills)) # O-N (in order of appearance)
-
-
 
 
 
--- omniply/core/gangs.py
+++ omniply/core/gangs.py
@@ -7,11 +7,25 @@ from .errors import GadgetFailure, ApplicationAmbiguityError
 from .gigs import GigBase
 
 
-
 class GangBase(GigBase, AbstractGang):
+	"""
+	The GangBase class is a subclass of GigBase and AbstractGang. It provides methods to handle gizmos and their
+	internal and external representations.
+
+	Attributes:
+	 _current_context (Optional[AbstractGig]): The current context of the gang.
+	"""
+
 	_current_context: Optional[AbstractGig]
 
 	def __init__(self, *, apply: Optional[dict[str, str]] = None, **kwargs):
+		"""
+		Initializes a new instance of the GangBase class.
+
+		Args:
+			apply (Optional[dict[str, str]]): A dictionary of gizmo mappings. If not provided, an empty dictionary will be used.
+			kwargs: Arbitrary keyword arguments.
+		"""
 		if apply is None:
 			apply = {}
 		super().__init__(**kwargs)
@@ -19,38 +33,70 @@ class GangBase(GigBase, AbstractGang):
 		self._raw_reverse_apply = None
 		self._current_context = None
 
-
 	def _gizmos(self) -> Iterator[str]:
-		'''lists gizmos produced by self (using internal names)'''
+		"""
+		Lists gizmos produced by self using internal names.
+
+		Returns:
+			Iterator[str]: An iterator over the gizmos.
+		"""
 		yield from super().gizmos()
 
-
 	def gizmos(self) -> Iterator[str]:
-		'''lists gizmos produced by self (using external names)'''
+		"""
+		Lists gizmos produced by self using external names.
+
+		Returns:
+			Iterator[str]: An iterator over the gizmos.
+		"""
 		for gizmo in self._gizmos():
 			yield self.gizmo_to(gizmo)
 
-
 	@property
 	def internal2external(self) -> dict[str, str]:
+		"""
+		Getter for the internal to external gizmo mapping.
+
+		Returns:
+			dict[str, str]: The internal to external gizmo mapping.
+		"""
 		return self._raw_apply
+
 	@internal2external.setter
 	def internal2external(self, value: dict[str, str]):
+		"""
+		Setter for the internal to external gizmo mapping.
+
+		Args:
+			value (dict[str, str]): The new internal to external gizmo mapping.
+		"""
 		self._raw_apply = value
 		self._raw_reverse_apply = None
 
-
 	@property
 	def external2internal(self) -> dict[str, str]:
-		# return self._infer_external2internal(self._raw_apply, self.gizmoto())
+		"""
+		Getter for the external to internal gizmo mapping.
+
+		Returns:
+			dict[str, str]: The external to internal gizmo mapping.
+		"""
 		if self._raw_reverse_apply is None:
 			self._raw_reverse_apply = self._infer_external2internal(self._raw_apply, self._gizmos())
 		return self._raw_reverse_apply
 
-
 	@staticmethod
-	# @lru_cache(maxsize=None)
 	def _infer_external2internal(raw: dict[str, str], products: Iterator[str]) -> dict[str, str]:
+		"""
+		Infers the external to internal gizmo mapping from the provided raw mapping and products.
+
+		Args:
+			raw (dict[str, str]): The raw gizmo mapping.
+			products (Iterator[str]): An iterator over the products.
+
+		Returns:
+			dict[str, str]: The inferred external to internal gizmo mapping.
+		"""
 		reverse = {}
 		for product in products:
 			if product in raw:
@@ -60,28 +106,60 @@ class GangBase(GigBase, AbstractGang):
 				reverse[external] = product
 		return reverse
 
-
 	def gizmo_from(self, gizmo: str) -> str:
+		"""
+		Converts an external gizmo to its internal representation.
+
+		Args:
+			gizmo (str): The external gizmo.
+
+		Returns:
+			str: The internal representation of the gizmo.
+		"""
 		return self.external2internal.get(gizmo, gizmo)
 
-
 	def gizmo_to(self, gizmo: str) -> str:
+		"""
+		Converts an internal gizmo to its external representation.
+
+		Args:
+			gizmo (str): The internal gizmo.
+
+		Returns:
+			str: The external representation of the gizmo.
+		"""
 		return self.internal2external.get(gizmo, gizmo)
 
-
 	def _grab_from_fallback(self, error: GadgetFailure, ctx: Optional[AbstractGig], gizmo: str) -> Any:
+		"""
+		Handles a GadgetFailure when trying to grab a gizmo from the context.
+
+		Args:
+			error (GadgetFailure): The GadgetFailure that occurred.
+			ctx (Optional[AbstractGig]): The context from which to grab the gizmo.
+			gizmo (str): The name of the gizmo to grab.
+
+		Returns:
+			Any: The result of the fallback operation.
+		"""
 		return super()._grab_from_fallback(error, self._current_context, self.gizmo_to(gizmo))
 
-
 	def grab_from(self, ctx: Optional[AbstractGig], gizmo: str) -> Any:
+		"""
+		Tries to grab a gizmo from the context.
+
+		Args:
+			ctx (Optional[AbstractGig]): The context from which to grab the gizmo.
+			gizmo (str): The name of the gizmo to grab.
+
+		Returns:
+			Any: The grabbed gizmo.
+		"""
 		if ctx is not None and ctx is not self and self._current_context is None:
 			assert self._current_context is None, f'Context already set to {self._current_context}'
 			self._current_context = ctx
 		if ctx is self._current_context:
-			gizmo = self.gizmo_from(gizmo) # convert to internal gizmo
+			gizmo = self.gizmo_from(gizmo)  # convert to internal gizmo
 		return super().grab_from(self, gizmo)
-
-
-
 
 
